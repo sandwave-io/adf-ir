d7e8dde0975795c58ed6111be40e05570285819a
Remy Adriaanse <remy@sandwave.io>
2023-12-18 16:25:52 +0100

    chore: upstream changes
      - Always include the JDK

--- SHIR/setup.ps1
+++ SHIR/setup.ps1
@@ -30,10 +30,18 @@ function RegisterNewNode {
         $AUTH_KEY,
         $NODE_NAME,
         $ENABLE_HA,
-        $HA_PORT
+        $HA_PORT,
+        $ENABLE_AE,
+        $AE_TIME
     )
 
     Write-Log "Start registering a new SHIR node"
+    Write-Log "Registering SHIR node with the node key: $($AUTH_KEY)"
+
+    if (!$NODE_NAME) {
+        $NODE_NAME = $Env:COMPUTERNAME
+    }
+    Write-Log "Registering SHIR node with the node name: $($NODE_NAME)"
 
     if ($ENABLE_HA -eq "true") {
         Write-Log "Enable High Availability"
@@ -46,8 +54,15 @@ function RegisterNewNode {
         Start-Sleep -Seconds 15
     }
 
-    if (!$NODE_NAME) {
-        Start-Process $DmgcmdPath -Wait -ArgumentList "-RegisterNewNode", "$($AUTH_KEY)" -RedirectStandardOutput "C:\SHIR\register-out.txt" -RedirectStandardError "C:\SHIR\register-error.txt"
+    if ($ENABLE_AE -eq "true") {
+        Write-Log "Enable Offline Nodes Auto-Expiration"
+        if (!$AE_TIME) {
+            $AE_TIME = 600
+        }
+
+        Write-Log "Node Expiration Time In Seconds: $($AE_TIME)"
+        Start-Process $DmgcmdPath -Wait -ArgumentList "-RegisterNewNode", "$($AUTH_KEY)", "$($NODE_NAME)", "$($AE_TIME)" -RedirectStandardOutput "C:\SHIR\register-out.txt" -RedirectStandardError "C:\SHIR\register-error.txt"
+        Start-Sleep -Seconds 15
     } else {
         Start-Process $DmgcmdPath -Wait -ArgumentList "-RegisterNewNode", "$($AUTH_KEY)", "$($NODE_NAME)" -RedirectStandardOutput "C:\SHIR\register-out.txt" -RedirectStandardError "C:\SHIR\register-error.txt"
     }
@@ -72,10 +87,10 @@ function RegisterNewNode {
 if (Check-Is-Registered) {
     Write-Log "Restart the existing node"
 
-    if ($ENABLE_HA -eq "true") {
+    if ((Test-Path Env:ENABLE_HA) -and ($Env:ENABLE_HA -eq "true")) {
         Write-Log "Enable High Availability"
-        $PORT = $HA_PORT
-        if (!$HA_PORT) {
+        $PORT = $Env:HA_PORT
+        if (!$Env:HA_PORT) {
             $PORT = "8060"
         }
         Write-Log "Remote Access Port: $($PORT)"
@@ -85,14 +100,9 @@ if (Check-Is-Registered) {
 
     Start-Process $DmgcmdPath -Wait -ArgumentList "-Start"
 } elseif (Test-Path Env:AUTH_KEY) {
-    Write-Log "Registering SHIR node with the node key: $($Env:AUTH_KEY)"
-    Write-Log "Registering SHIR node with the node name: $($Env:NODE_NAME)"
-    Write-Log "Registering SHIR node with the enable high availability flag: $($Env:ENABLE_HA)"
-    Write-Log "Registering SHIR node with the tcp port: $($Env:HA_PORT)"
-    
     Start-Process $DmgcmdPath -Wait -ArgumentList "-Start"
 
-    RegisterNewNode $Env:AUTH_KEY $Env:NODE_NAME $Env:ENABLE_HA $Env:HA_PORT
+    RegisterNewNode $Env:AUTH_KEY $Env:NODE_NAME $Env:ENABLE_HA $Env:HA_PORT $Env:ENABLE_AE $Env:AE_TIME
 } else {
     Write-Log "Invalid AUTH_KEY Value"
     exit 1
